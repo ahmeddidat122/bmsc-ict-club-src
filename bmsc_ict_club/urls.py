bc5313a8fdf50347985aa5b5ddcb2d1f50857463
ahmedddidat112 <salat12287xd@gmail.com>
2025-08-08 12:51:02 +0600

    phase 2 completed

--- bmsc_ict_club/urls.py
+++ bmsc_ict_club/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('about/', views.About, name='about'),
+    path('events/', views.events, name='events'),
+    path('blogs/', views.blogs, name='blogs'),
+    path('contact/', views.contact, name='contact'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'core/home.html', {'year' : datetime.now().year})
+
+def About(request):
+    return render(request, 'core/about.html', {'year' : datetime.now().year})
+
+def events(request):
+    return render(request, 'core/events.html', {'year' : datetime.now().year})
+
+def blogs(request):
+    return render(request, 'core/blogs.html', {'year' : datetime.now().year})
+
+def contact(request):
+    return render(request, 'core/contact.html', {'year' : datetime.now().year})
